58fd305b23719866ffb82031e255edf08c96098f
Ruslan_L <rus059111@gmailcom>
2023-01-20 19:07:13 +0600

    first decision

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+//Задание 2. Сумма двух чисел по единице
+
+import (
+	"fmt"
+	//"math"
+)
+
+func main() {
+
+	var number1 int
+	var number2 int
+
+	fmt.Println("Введите число 1")
+	fmt.Scan(&number1)
+	fmt.Println("Введите число 2")
+	fmt.Scan(&number2)
+
+	j := number1
+	c := number1 + number2
+
+	for {
+		j = j + 1
+		if j == c {
+			fmt.Println(j)
+			break
+		}
+	}
+}
